2e6be17cad5d903696eadec9df8f238da995ebcb
ari-dv <rosalesariana102@gmail.com>
2026-04-26 18:48:00 -0500

    prueba 5.9

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown("""
 # --- 2. CARGA DE DATOS Y LIMPIEZA CRÍTICA ---
 @st.cache_data
 def load_data():
-    ruta = 'data/resultados.csv'
+    ruta = 'data/DataLimpia.csv'
     if os.path.exists(ruta):
         df = pd.read_csv(ruta)
         df['fecha'] = pd.to_datetime(df['fecha'])
